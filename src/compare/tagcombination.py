2221ff2aa5ebee572f0cfc2fa887f337a09fbcc2
beowulfscat <sam.venus@gmx.de>
2013-07-03 12:38:19 +0200

    Now with file selection and evaluation!
    New. added command line options (select tagger output file and gold
    standard file); added Precision, Recall, and F score evaluation; also it
    is now possible to compare files with different number of columns (up to
    9).

--- src/compare/tagcombination.py
+++ src/compare/tagcombination.py
@@ -16,6 +16,26 @@ class TagCombination:
         def __eq__(self, other):
                 #self.__dict___ --> all attributes of object
                 #as dict (associative array)
-                return self.__dict__ == other.__dict__
+                correct = True
+                if other.pos != self.pos: #pos is required
+                        return False
+                elif self.case is not None and other.case is not None and other.case != self.case:
+                        return False
+                elif self.number is not None and other.number is not None and other.number != self.number:
+                        return False
+                elif self.gender is not None and other.gender is not None and other.gender != self.gender:
+                        return False
+                elif self.person is not None and other.person is not None and other.person != self.person:
+                        return False
+                elif self.time is not None and other.time is not None and other.time != self.time:
+                        return False
+                elif self.mode is not None and other.mode is not None and other.mode != self.mode:
+                        return False
+                elif self.genusVerbi is not None and other.genusVerbi is not None and other.genusVerbi != self.genusVerbi:
+                        return False
+                else:
+                        return True
+                
+                #return self.__dict__ == other.__dict__
 
         
